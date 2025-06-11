879becd2ff408869a40ce8c6332b12af38396b9a
Joris Ebbelaar <joris@datalumina.com>
2025-06-11 11:58:40 +0200

    Add async to process method

--- app/core/commands/init_workflow.py
+++ app/core/commands/init_workflow.py
@@ -56,7 +56,7 @@ from core.task import TaskContext
 
 
 class InitialNode(Node):
-    def process(self, task_context: TaskContext) -> TaskContext:
+    async def process(self, task_context: TaskContext) -> TaskContext:
         return task_context
             """)
         else:
